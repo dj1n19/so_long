2b293476f2539c6e9ca46695e374e8752f2833b6
Bruno Genie <bgenie@shi-r9-p5.s19.be>
2022-05-20 17:23:59 +0200

    change textures path

--- srcs/load_textures.c
+++ srcs/load_textures.c
@@ -22,13 +22,13 @@ static void	ft_load_corners(t_datas *datas)
 	width = TILESIZE;
 	height = TILESIZE;
 	datas->map->edges[0] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/corner_top_left.xpm", &width, &height);
+			"assets/corner_top_left.xpm", &width, &height);
 	datas->map->edges[2] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/corner_top_right.xpm", &width, &height);
+			"assets/corner_top_right.xpm", &width, &height);
 	datas->map->edges[4] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/corner_bottom_right.xpm", &width, &height);
+			"assets/corner_bottom_right.xpm", &width, &height);
 	datas->map->edges[6] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/corner_bottom_left.xpm", &width, &height);
+			"assets/corner_bottom_left.xpm", &width, &height);
 	while (i < 8)
 	{
 		if (!datas->map->edges[i])
@@ -49,13 +49,13 @@ static void	ft_load_edges(t_datas *datas)
 	width = TILESIZE;
 	height = TILESIZE;
 	datas->map->edges[1] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/edge_top.xpm", &width, &height);
+			"assets/edge_top.xpm", &width, &height);
 	datas->map->edges[3] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/edge_right.xpm", &width, &height);
+			"assets/edge_right.xpm", &width, &height);
 	datas->map->edges[5] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/edge_bottom.xpm", &width, &height);
+			"assets/edge_bottom.xpm", &width, &height);
 	datas->map->edges[7] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/edge_left.xpm", &width, &height);
+			"assets/edge_left.xpm", &width, &height);
 	while (i < 8)
 	{
 		if (!datas->map->edges[i])
@@ -71,7 +71,7 @@ static void	ft_load_item(t_datas *datas)
 
 	width = TILESIZE;
 	height = TILESIZE;
-	datas->map->item = mlx_xpm_file_to_image(datas->mlx, "../assets/chest.xpm",
+	datas->map->item = mlx_xpm_file_to_image(datas->mlx, "assets/chest.xpm",
 			&width, &height);
 	if (!datas->map->item)
 		ft_e_load(2, datas);
@@ -87,13 +87,13 @@ static void	ft_load_player(t_datas *datas)
 	width = TILESIZE;
 	height = TILESIZE;
 	datas->player->player[0] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/player_up.xpm", &width, &height);
+			"assets/player_up.xpm", &width, &height);
 	datas->player->player[1] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/player_right.xpm", &width, &height);
+			"assets/player_right.xpm", &width, &height);
 	datas->player->player[2] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/player_down.xpm", &width, &height);
+			"assets/player_down.xpm", &width, &height);
 	datas->player->player[3] = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/player_left.xpm", &width, &height);
+			"assets/player_left.xpm", &width, &height);
 	datas->player->current = datas->player->player[1];
 	while (i < 4)
 	{
@@ -116,13 +116,13 @@ void	ft_load_textures(t_datas *datas)
 	ft_load_item(datas);
 	ft_load_player(datas);
 	datas->map->ground = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/grass.xpm", &width, &height);
-	datas->map->wall = mlx_xpm_file_to_image(datas->mlx, "../assets/trees.xpm",
+			"assets/grass.xpm", &width, &height);
+	datas->map->wall = mlx_xpm_file_to_image(datas->mlx, "assets/trees.xpm",
 			&width, &height);
 	datas->map->exit_open = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/portal_open.xpm", &width, &height);
+			"assets/portal_open.xpm", &width, &height);
 	datas->map->exit_close = mlx_xpm_file_to_image(datas->mlx,
-			"../assets/portal_close.xpm", &width, &height);
+			"assets/portal_close.xpm", &width, &height);
 	if (!datas->map->wall || !datas->map->ground || !datas->map->exit_open
 		|| !datas->map->exit_close)
 	{
