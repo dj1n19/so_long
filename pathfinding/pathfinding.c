cbb063afa4891d3feb6466120170b79fa0fa1ab1
dj1n19 <bruno.genie@hotmail.com>
2022-11-09 23:52:03 +0100

    fuck the norm

--- pathfinding/pathfinding.c
+++ pathfinding/pathfinding.c
@@ -68,41 +68,75 @@ unsigned int **get_cost_map(t_map *map)
     return cost_map;
 }
 
-t_list    *get_next(t_map *map, unsigned int **cost_map, t_list *node, t_list *head)
+static unsigned long	get_next_x(t_map *map, unsigned int **cost_map, t_list *node, t_list *head)
 {
-    unsigned int    lower_cost;
-    unsigned long   next;
-    t_list          *next_node;
+	unsigned int	lower_cost;
+	unsigned long	next;
 
-    lower_cost = UINT_MAX;
-    next = 0;
-    if (!include(node->x - 1, node->y, head) && cost_map[node->y][node->x - 1] < lower_cost && map->map[node->y][node->x - 1] != '1')
+	lower_cost = UINT_MAX;
+	next = 0;
+	if (!include(node->x - 1, node->y, head)
+		&& cost_map[node->y][node->x - 1] < lower_cost
+		&& map->map[node->y][node->x - 1] != '1')
     {
         lower_cost = cost_map[node->y][node->x - 1];
         next = ((unsigned long)node->y << 32);
         next += (unsigned long)(node->x - 1);
     }
-    if (!include(node->x + 1, node->y, head) && cost_map[node->y][node->x + 1] < lower_cost && map->map[node->y][node->x + 1] != '1')
+    if (!include(node->x + 1, node->y, head)
+		&& cost_map[node->y][node->x + 1] < lower_cost
+		&& map->map[node->y][node->x + 1] != '1')
     {
         lower_cost = cost_map[node->y][node->x + 1];
         next = ((unsigned long)node->y << 32);
         next += (unsigned long)(node->x + 1);
     }
-    if (!include(node->x, node->y - 1, head) && cost_map[node->y - 1][node->x] < lower_cost && map->map[node->y - 1][node->x] != '1')
+	return (next);
+}
+
+static unsigned long	get_next_y(t_map *map, unsigned int **cost_map, t_list *node, t_list *head)
+{
+	unsigned int	lower_cost;
+	unsigned long	next;
+
+	lower_cost = UINT_MAX;
+	next = 0;
+	if (!include(node->x, node->y - 1, head)
+		&& cost_map[node->y - 1][node->x] < lower_cost
+		&& map->map[node->y - 1][node->x] != '1')
     {
         lower_cost = cost_map[node->y - 1][node->x];
         next = ((unsigned long)(node->y - 1) << 32);
         next += (unsigned long)node->x;
     }
-    if (!include(node->x, node->y + 1, head) && cost_map[node->y + 1][node->x] < lower_cost && map->map[node->y + 1][node->x] != '1')
+    if (!include(node->x, node->y + 1, head)
+		&& cost_map[node->y + 1][node->x] < lower_cost
+		&& map->map[node->y + 1][node->x] != '1')
     {
         lower_cost = cost_map[node->y + 1][node->x];
         next = ((unsigned long)(node->y + 1) << 32);
         next += (unsigned long)node->x;
     }
-    if (next != 0)
-        return (create_node((unsigned int)next, (unsigned int)(next >> 32)));
-    return (NULL);
+	return (next);
+}
+
+t_list    *get_next(t_map *map, unsigned int **cost_map, t_list *node, t_list *head)
+{
+    unsigned int    lower_cost;
+    unsigned long   next_x;
+	unsigned long	next_y;
+	t_list			*next_node;
+
+    next_x = get_next_x(map, cost_map, node, head);
+	next_y = get_next_y(map, cost_map, node, head);
+	if (next_x == 0 && next_y == 0)
+		return (NULL);
+	if (cost_map[(unsigned int)(next_x >> 32)][(unsigned int)next_x]
+		< cost_map[(unsigned int)(next_y >> 32)][(unsigned int)next_y])
+		next_node = create_node((unsigned int)next_x, (unsigned int)(next_x >> 32));
+	else
+		next_node = create_node((unsigned int)next_y, (unsigned int)(next_y >> 32));
+	return (next_node);
 }
 
 int has_next(t_map *map, unsigned int x, unsigned int y)
@@ -160,9 +194,9 @@ int pathfinding(t_map *map, unsigned int px, unsigned int py)
             meta = pop_back(meta);
             next = meta->tail;
         }
-        print_map(map, meta->tail->x, meta->tail->y);
-        usleep(250000);
-        system("clear");
+        // print_map(map, meta->tail->x, meta->tail->y);
+        // usleep(250000);
+        // system("clear");
     }
     return 0;
 }
